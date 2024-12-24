b85510a1b977d4bf3451fa82f6a56eea942af22c
Piotr Kaczmarski <piotr.kaczmarski.ext@assetmax.ch>
2024-12-24 12:18:00 +0100

    Lua ls folke plugin for vim globals

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -14,6 +14,17 @@ return {
 
       -- Schema information
       "b0o/SchemaStore.nvim",
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
     },
     config = function()
       -- Lsp to completions
