6d16a560e68048d3ec897f19cb443d9a2ad1e5fb
Potato <potato@potatogam.es>
2025-01-21 20:33:53 +0100

    space => leader

--- lua/custom/telescope.lua
+++ lua/custom/telescope.lua
@@ -23,10 +23,9 @@ pcall(require("telescope").load_extension, "ui-select")
 
 local builtin = require "telescope.builtin"
 
---TODO: Maybe change f to t like telescope?
---Also, leader instead of space.
 vim.keymap.set("n", "<leader>td", builtin.find_files)
 vim.keymap.set("n", "<leader>tt", builtin.git_files)
+
 vim.keymap.set("n", "<leader>th", builtin.help_tags)
 vim.keymap.set("n", "<leader>tg", builtin.live_grep)
 vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find)
@@ -37,17 +36,3 @@ vim.keymap.set("n", "<leader>ta", function()
   ---@diagnostic disable-next-line: param-type-mismatch
   builtin.find_files { cwd = vim.fs.joinpath(vim.fn.stdpath "data", "lazy") }
 end)
-
---[[
-vim.keymap.set("n", "<space>en", function()
-  builtin.find_files { cwd = vim.fn.stdpath "config" }
-end)
-
-vim.keymap.set("n", "<space>eo", function()
-  builtin.find_files { cwd = "~/.config/nvim-backup/" }
-end)
-
-vim.keymap.set("n", "<space>fp", function()
-  builtin.find_files { cwd = "~/plugins/" }
-end)
---]]
